b05391f0055a36a226ba7bf63b77dcaaae74ee1a
anandmiskin <73897496+anandmiskin@users.noreply.github.com>
2020-11-07 11:30:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//Accordian section
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
+
+    
